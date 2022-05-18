609e041c7e3ce28db4af5e08112511a6f2650b32
alperdegre <alper.degre.dev@gmail.com>
2022-05-18 20:51:52 +0300

    Finished HeaderImage component

--- src/components/MainPage/HeaderImage.js
+++ src/components/MainPage/HeaderImage.js
@@ -2,7 +2,23 @@ import React from 'react';
 import classes from './HeaderImage.module.css';
 
 function HeaderImage() {
-  return <div className={classes['header-image']}>Img</div>;
+  return (
+    <div className={classes['header-image']}>
+      <div className="container">
+        <div className={classes['header-text']}>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
+            asperiores nemo sapiente earum sit, accusantium sequi laboriosam sed
+            temporibus quidem! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Quidem et ullam aut nisi nam totam quod temporibus
+            cum, cumque natus?
+          </p>
+          <button type="button">Shop Now</button>
+        </div>
+        <p>Test</p>
+      </div>
+    </div>
+  );
 }
 
 export default HeaderImage;
