72a99a46c33c716476a78db8153bd90ee951ac43
alperdegre <alper.degre.dev@gmail.com>
2022-05-21 19:42:46 +0300

    Add test fixes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import classes from './Header.module.css';
 import { ReactComponent as Logo } from '../../assets/icons/coffee.svg';
 import HeaderButton from './HeaderButton';
 
 function Header() {
+  const { pathname } = useLocation();
+  const [navbarClasses, setNavbarClasses] = useState(`${classes.navbar}`);
+
+  useEffect(() => {
+    switch (pathname) {
+      case '/':
+        setNavbarClasses(`${classes.navbar}`);
+        break;
+      case '/products':
+        setNavbarClasses(`${classes.navbar} ${classes.navbar_products}`);
+        break;
+      default:
+        setNavbarClasses(`${classes.navbar}`);
+        break;
+    }
+  }, [pathname]);
+
   return (
-    <header className={classes.navbar}>
+    <header className={navbarClasses}>
       <button className={classes.logo} type="button">
         <Logo />
         <h1>COFFEE SHOP</h1>
