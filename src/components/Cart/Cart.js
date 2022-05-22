d3bcb933e1041ce377f2e49f17bf26b94a100bbf
alperdegre <alper.degre.dev@gmail.com>
2022-05-23 00:25:21 +0300

    Add Cart component styles

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
 
 function Cart({ onClose }) {
+  const checkoutHandler = () => {};
   return (
     <Modal onClose={onClose}>
-      <h2>Cart</h2>
-      <p>Items</p>
-      <h2>Close</h2>
-      <h2>Order</h2>
+      <div className={classes['cart-title']}>SHOPPING CART</div>
+      {/* Here will be a check like 
+      cartContext.items.length !== 0 ? <Show Cart Items> : <Show No Items Div> 
+      After we add context for cart items
+      */}
+      <p className={classes['cart-no-items']}>
+        There are no items in your cart
+      </p>
+      <div className={classes['cart-total']}>
+        <div className={classes['cart-total-title']}>Total</div>
+        <div>$ 50.00</div>
+      </div>
+      <div className={classes['cart-controls']}>
+        <div
+          role="button"
+          className={classes['cart-button']}
+          onClick={onClose}
+          onKeyDown={onClose}
+          tabIndex={0}
+        >
+          CLOSE
+        </div>
+        <div
+          role="button"
+          className={classes['cart-button']}
+          onClick={checkoutHandler}
+          onKeyDown={checkoutHandler}
+          tabIndex={0}
+        >
+          ORDER
+        </div>
+      </div>
     </Modal>
   );
 }
