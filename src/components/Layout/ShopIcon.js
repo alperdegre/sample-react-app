c0cd3bd5658e9fc0cf902cc37c9736be4c66b1f0
alperdegre <alper.degre.dev@gmail.com>
2022-05-23 19:01:16 +0300

    Add badge to show cart items

--- src/components/Layout/ShopIcon.js
+++ src/components/Layout/ShopIcon.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { ReactComponent as ShoppingCart } from '../../assets/icons/shopping-cart.svg';
 import classes from './ShopIcon.module.css';
+import CartContext from '../../store/cart-context';
 
 function ShopIcon({ onShowCart }) {
+  const { items } = useContext(CartContext);
+  const numberOfCartItems = items.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
+  const [badgeBump, setBadgeBump] = useState(false);
+  const [badgeShow, setBadgeShow] = useState(false);
+  const badgeClasses = `${classes.badge} ${
+    badgeBump ? classes['badge-bump'] : ''
+  } ${badgeShow ? classes['badge-show'] : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      setBadgeShow(false);
+    } else {
+      setBadgeShow(true);
+    }
+    setBadgeBump(true);
+
+    const timeout = setTimeout(() => {
+      setBadgeBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [items, setBadgeBump, setBadgeShow, numberOfCartItems]);
+
   return (
     <div
       className={classes['shop-icon']}
@@ -13,6 +41,7 @@ function ShopIcon({ onShowCart }) {
       tabIndex={0}
       aria-label="Open cart"
     >
+      <div className={badgeClasses}>{numberOfCartItems}</div>
       <ShoppingCart />
     </div>
   );
